5bd29a55b2c8c7588e76d50e16d6d2929be32d17
Evy Sanchez <evysanzz500@gmail.com>
2025-10-20 17:33:18 -0700

    get earliest and latest tweets and update DOM

--- js/about.js
+++ js/about.js
@@ -8,10 +8,30 @@ function parseTweets(runkeeper_tweets) {
 	tweet_array = runkeeper_tweets.map(function(tweet) {
 		return new Tweet(tweet.text, tweet.created_at);
 	});
+
+	earliest_tweet = tweet_array[0];
+	latest_tweet = tweet_array[0];
+	for(var i = 0; i < tweet_array.length; i++){
+		if(tweet_array[i].time < earliest_tweet.time){
+			earliest_tweet = tweet_array[i];
+		}
+		if(tweet_array[i].time > latest_tweet.time){
+			latest_tweet = tweet_array[i];
+		}
+	}
+
+	const options = {
+		weekday: "long",
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+	};
 	
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
 	document.getElementById('numberTweets').innerText = tweet_array.length;	
+	document.getElementById('firstDate').innerText = earliest_tweet.time.toLocaleDateString('en-US', options);
+	document.getElementById('lastDate').innerText = latest_tweet.time.toLocaleDateString('en-US', options);
 }
 
 //Wait for the DOM to load
