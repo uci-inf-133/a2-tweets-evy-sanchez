6be0ff119cfb559bdd7089360b875775a2f2ab15
Evy Sanchez <evysanzz500@gmail.com>
2025-10-29 12:29:25 -0700

    working and much more efficient

--- js/descriptions.js
+++ js/descriptions.js
@@ -13,35 +13,41 @@ function parseTweets(runkeeper_tweets) {
 	});
 
 	//TODO: Filter to just the written tweets
-	
 	for(var i = 0; i < tweet_array.length; i++){
 		//console.log(tweet_array[i].written);
 		if(tweet_array[i].written){
 			written_tweets.push(tweet_array[i]);
 		}
 	}
-	//console.log(written_tweets);
 }
 
 function addEventHandlerForSearch() {
-	//console.log(written_tweets);
 	//TODO: Search the written tweets as text is entered into the search box, and add them to the table
 	//every time user types:
-	table = document.getElementById('tweetTable');
-	console.log(table.innerHTML);
+	let table = document.getElementById('tweetTable');
 	table.innerHTML = "";
+	var index;
+	let htmlString = "";
 	function search_and_update(input){
-		console.log(written_tweets);
-		var index = 0;
-		for(var i = 0; i < written_tweets.length; i++){
-			if(written_tweets[i].text.toLowerCase().includes(input.toLowerCase())){
-				index++;
-				table.innerHTML += (written_tweets[i].getHTMLTableRow(index));
+		htmlString = "";
+		index = 0;
+		if(input.trim().length>0){
+			const loweredInput = input.toLowerCase();
+			for(var i = 0; i < written_tweets.length; i++){
+				if(written_tweets[i].writtenText.toLowerCase().includes(loweredInput)){
+					console.log(written_tweets[i].text);
+					console.log(loweredInput);
+					index++;
+					htmlString += (written_tweets[i].getHTMLTableRow(index));
+				}
 			}
 		}
+		table.innerHTML = htmlString;
+		document.getElementById("searchCount").innerHTML = index;
+		document.getElementById("searchText").innerHTML = input;
 	}
 	textFilter = document.getElementById("textFilter");
-	textFilter.addEventListener('input', function(){search_and_update(textFilter.value.trim())});
+	textFilter.addEventListener('input', function(){search_and_update(textFilter.value)});
 }
 
 //Wait for the DOM to load
